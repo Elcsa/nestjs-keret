7faffdc48d9540ecea78d94db003d99011c56a98
Elcsa <81393593+Elcsa@users.noreply.github.com>
2023-01-12 16:56:37 +0100

    Main

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,14 @@
 import {
+  Body,
   Controller,
   Get,
-  Render,
+  Param, Query,
+  Render,Redirect, Post,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import db from './db';
+import { Books }from './books.dto';
+
 
 @Controller()
 export class AppController {
@@ -12,7 +16,38 @@ export class AppController {
 
   @Get()
   @Render('index')
-  index() {
-    return { message: 'Welcome to the homepage' };
+  async listbooks(){
+    const [rows]=await db.execute(
+      'SElect id,title rating from  books where 1 order by rating desc;',
+
+    );
+    return{
+      Books:rows,
+    };
+  }
+  @Get('books/new')
+  @Render('form')
+  newkonyvekForm(){
+    return{};
+  }
+  @Get('/keres')
+  @Render('index')
+  async booksSearch(@Query('rating')rating:number){
+    const[rows]=await db.execute(
+      'SElect id,title,rating from books where rating like?'[rating],
+    );
+    return{Books: new};
+  }
+  @Post('books/new')
+  @Redirect()
+  async newBooks(@Body()Books:Books){
+    const[result]:any=await db.execute(
+      'insert into books (title,rating)values(?,?)',
+      [Books.title,Books.rating],
+    );
+    return{
+      url:'/',
+    };
   }
 }
+ 
